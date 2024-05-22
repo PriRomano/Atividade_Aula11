aad6b1e159ee70cc05da2a698432682b6280c7e0
Priscila Romano <161603421+PriRomano@users.noreply.github.com>
2024-05-22 02:04:39 -0300

    Update Mensalista.java

--- src/Mensalista.java
+++ src/Mensalista.java
@@ -1,3 +1,30 @@
-public class Mensalista {
-    
+public class Mensalista  extends Empregado {
+   
+    private String cargo;
+ 
+    public String getCargo () {
+        return cargo;
+    }
+ 
+    public void setCargo (String cargo) {
+        this.cargo = cargo;
+    }
+ 
+    //Construtor
+    public Mensalista (String nome, String endereco, double salario, String cargo) {
+        super (nome, endereco, salario);
+        this.cargo = cargo;
+    }
+ 
+    public void calcularSalario () {
+        if (cargo.equals ("Junior")) {
+            salario = 2500;
+        } else if (cargo.equals ("Pleno")) {
+            salario = 3500;
+        } else {
+            salario = 5500;
+        }
+    }
+ 
+ 
 }
